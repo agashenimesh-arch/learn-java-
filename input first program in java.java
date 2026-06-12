5d1aa2df8e8a94f93858103dd4f14ac51ac3edde
Nimesh Agashe <232766002+agashenimesh-arch@users.noreply.github.com>
2026-06-12 02:41:51 -0700

    Create input first program in java.java

---
+++ input first program in java.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+   firstprograminjava_output_and_input_learn() {
+   
+
+   public static void main(String[] var0) {
+      System.out.println("Hello, World!");
+      Scanner var1 = new Scanner(System.in);
+      System.out.print("Enter your name: ");
+      String var2 = var1.nextLine();
+      System.out.println("Hello, " + var2 + "!");
+      var1.close();
+      delect  firstprograminjava_output_and_input_learn();
+   }
+}
